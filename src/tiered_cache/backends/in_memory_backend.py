f72668e0f3c99807120e39259c9a2e70b387e44a
Mirhossein <mr.mousavi@live.com>
2025-07-21 21:19:27 +0200

    implement LRU eviction policy in InMemoryBackend

--- src/tiered_cache/backends/in_memory_backend.py
+++ src/tiered_cache/backends/in_memory_backend.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 
 
 class InMemoryBackend(CacheBackend):
-    """Implements in-memory cache. remove oldest added, when hits max_size."""
+    """Implements in-memory cache using LRU eviction policy."""
 
     cache = OrderedDict()
 
@@ -13,12 +13,20 @@ class InMemoryBackend(CacheBackend):
         self.max_size = max_size
 
     def set(self, key: str, value: Any):
-        if self.max_size and len(self.cache) > self.max_size:
-            self.cache.popitem(last=False)
-        self.cache[key] = value
+        if key in self.cache:
+            # Update and move to end
+            self.cache.move_to_end(key)
+        try:
+            self.cache[key] = value
+        finally:
+            if self.max_size and len(self.cache) > self.max_size:
+                self.cache.popitem(last=False)
+            self.cache[key] = value
 
     def get(self, key: str):
-        with ErrorHandler(KeyError):
+        if key in self.cache:
+            # Move key to end to show that it was recently used
+            self.cache.move_to_end(key)
             return self.cache[key]
 
     def remove(self, key: str):
